b7b4b670cb0e1bb05a4460c32ab79bf367681f3b
Jeremie Jost <jeremiejost@gmail.com>
2023-01-24 14:39:24 +0000

    Ukulele config

--- karplus-strong/guitar.js
+++ karplus-strong/guitar.js
@@ -9,34 +9,32 @@ function Guitar(audioCtx, audioDestination) {
         // - string number
         // - octave
         // - semitone
-        new GuitarString(audioCtx, audioDestination, 0, 2, 4),   // E2
-        new GuitarString(audioCtx, audioDestination, 1, 2, 9),   // A2
-        new GuitarString(audioCtx, audioDestination, 2, 3, 2),   // D3
-        new GuitarString(audioCtx, audioDestination, 3, 3, 7),   // G3
-        new GuitarString(audioCtx, audioDestination, 4, 3, 11),  // B3
-        new GuitarString(audioCtx, audioDestination, 5, 4, 4)    // E4
+        new GuitarString(audioCtx, audioDestination, 0, 4, 7),   // D3
+        new GuitarString(audioCtx, audioDestination, 1, 4, 0),   // G3
+        new GuitarString(audioCtx, audioDestination, 2, 4, 4),  // B3
+        new GuitarString(audioCtx, audioDestination, 3, 4, 9)    // E4
     ];
 }
 
 // each fret represents an increase in pitch by one semitone
 // (logarithmically, one-twelth of an octave)
 // -1: don't pluck that string
-Guitar.C_MAJOR = [-1,  3, 2, 0, 0, 0];
-Guitar.G_MAJOR = [ 3,  2, 0, 0, 0, 3];
-Guitar.A_MINOR = [ 0,  0, 2, 2, 0, 0];
-Guitar.E_MINOR = [ 0,  2, 2, 0, 3, 0];
+Guitar.C_MAJOR = [0, 0, 0, 3];
+Guitar.G_MAJOR = [0, 2, 3, 2];
+Guitar.A_MINOR = [2, 0, 0, 0];
+Guitar.E_MINOR = [2, 0, 1, 0];
 
 // to add a class method in JavaScript,
 // we add a function property to the class's 'prototype' property
 Guitar.prototype.strumChord = function(time, downstroke, velocity, chord) {
     var pluckOrder;
     if (downstroke === true) {
-        pluckOrder = [0, 1, 2, 3, 4, 5];
+        pluckOrder = [0, 1, 2, 3];
     } else {
-        pluckOrder = [5, 4, 3, 2, 1, 0];
+        pluckOrder = [3, 2, 1, 0];
     }
 
-    for (var i = 0; i < 6; i++) {
+    for (var i = 0; i < 4; i++) {
         var stringNumber = pluckOrder[i];
         if (chord[stringNumber] != -1) {
             this.strings[stringNumber].pluck(time, velocity, chord[stringNumber]);
@@ -47,7 +45,7 @@ Guitar.prototype.strumChord = function(time, downstroke, velocity, chord) {
 };
 
 Guitar.prototype.setMode = function(mode) {
-    for (var i = 0; i < 6; i++) {
+    for (var i = 0; i < 4; i++) {
         this.strings[i].mode = mode;
     }
 };
